8bdf3ebbf40e852e43c848bcffb7cfaacc05ae97
Bertil Andersen <bertilandersen@hotmail.com>
2018-10-26 18:01:54 +0200

    opgave 2 done

---
+++ uge1Opgave2/Compairer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace uge1Opgave2
+{
+    class Compairer
+    {
+        static char[] one;
+        static char[] two;
+        static bool isAcronym = false;
+
+        public static bool compairStrings(string a, string b)
+        {
+            one = a.ToCharArray();
+            two = b.ToCharArray();
+            Array.Sort(one);
+            Array.Sort(two);
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (one[i] == two[i])
+                {
+                    isAcronym = true;
+                }
+                else
+                {
+                    isAcronym = false;
+                    break;
+                }
+            }
+
+            return isAcronym;
+        }
+    }
+}
--- uge1Opgave2/Program.cs
+++ uge1Opgave2/Program.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Lav metode til at sammenligne to strenge for akronym
+
 namespace uge1Opgave2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            string a;
+            string b;
+            bool answer = false;
+
+            Console.WriteLine("Enter first string here: ");
+            a = Console.ReadLine();
+            Console.WriteLine("Enter second string here: ");
+            b = Console.ReadLine();
+
+            answer = Compairer.compairStrings(a, b);
+            Console.WriteLine("The two strings match: " + answer.ToString());
+
+            Console.ReadKey();
         }
     }
 }
